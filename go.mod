f105c974ccc753c00142d6b1f54efc65bf1dc4bb
Eric Fritz <eric@eric-fritz.com>
2019-06-24 21:18:33 -0500

    Update deps and regenerate mocks.

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/efritz/gache
 
+go 1.12
+
 require (
-	github.com/aphistic/sweet v0.0.0-20180618201346-68e18ab55a67
-	github.com/aphistic/sweet-junit v0.0.0-20171005212431-6b78f7014f7c
+	github.com/aphistic/sweet v0.2.0
+	github.com/aphistic/sweet-junit v0.0.0-20190314030539-8d7e248096c2
 	github.com/efritz/deepjoy v0.0.0-20181228234358-6d5e5c61e1b3
-	github.com/efritz/go-mockgen v0.0.0-20181025151746-efd6a0b3e163
+	github.com/efritz/go-mockgen v0.0.0-20190613153341-3425cf558834
 	github.com/garyburd/redigo v1.6.0
-	github.com/onsi/gomega v1.4.3
+	github.com/onsi/gomega v1.5.0
 )
--- redis_test.go
+++ redis_test.go
@@ -1,21 +1,18 @@
 package gache
 
-//go:generate go-mockgen github.com/efritz/deepjoy -i Client -d mock -f
-
 import (
 	"time"
 
 	"github.com/aphistic/sweet"
+	"github.com/efritz/deepjoy/mocks"
 	. "github.com/efritz/go-mockgen/matchers"
 	. "github.com/onsi/gomega"
-
-	"github.com/efritz/gache/mock"
 )
 
 type RedisSuite struct{}
 
 func (s *RedisSuite) TestGetValue(t sweet.T) {
-	client := mock.NewMockClient()
+	client := mocks.NewMockClient()
 	client.ReadReplicaFunc.SetDefaultReturn(client)
 	client.DoFunc.SetDefaultReturn([]uint8{'b', 'a', 'r'}, nil)
 
@@ -26,7 +23,7 @@ func (s *RedisSuite) TestGetValue(t sweet.T) {
 }
 
 func (s *RedisSuite) TestSetValue(t sweet.T) {
-	client := mock.NewMockClient()
+	client := mocks.NewMockClient()
 	cache := NewRedisCache(client, WithRedisPrefix("prefix"), WithRedisTTL(time.Second*120))
 	Expect(cache.SetValue("foo", "bar", "x", "y", "z")).To(BeNil())
 	Expect(client.DoFunc).To(BeCalledOnceWith(
@@ -43,14 +40,14 @@ func (s *RedisSuite) TestSetValue(t sweet.T) {
 }
 
 func (s *RedisSuite) TestRemove(t sweet.T) {
-	client := mock.NewMockClient()
+	client := mocks.NewMockClient()
 	cache := NewRedisCache(client, WithRedisPrefix("prefix"), WithRedisTTL(time.Second*120))
 	Expect(cache.Remove("foo")).To(BeNil())
 	Expect(client.DoFunc).To(BeCalledOnceWith("DEL", "prefix.foo"))
 }
 
 func (s *RedisSuite) TestBustTags(t sweet.T) {
-	client := mock.NewMockClient()
+	client := mocks.NewMockClient()
 	cache := NewRedisCache(client, WithRedisPrefix("prefix"))
 	Expect(cache.BustTags("x", "y", "z")).To(BeNil())
 	Expect(client.DoFunc).To(BeCalledOnceWith(
